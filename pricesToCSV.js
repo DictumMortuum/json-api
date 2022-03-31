6d060b4fd198320d96dbc5b4d82ed1495ca1b60d
DictumMortuum <theio.vrefos@gmail.com>
2022-03-31 13:17:46 +0300

    added stock and store to the historic prices script

--- pricesToCSV.js
+++ pricesToCSV.js
@@ -1,42 +1,14 @@
-const data = require("./prices.json")
+const data = require("/home/dimitris/Code/DictumMortuum/json-api/prices.json")
 const util = require('util');
-
-const rs = {}
-
-// data.map(d => {
-//   if (d.boardgame_id === null) {
-//     return
-//   }
-
-//   if (rs[d.boardgame_id] === undefined) {
-//     rs[d.boardgame_id] = []
-//   }
-
-//   rs[d.boardgame_id].push({
-//     cr_date: d.cr_date,
-//     price: d.price
-//   })
-// })
-
-// console.log(JSON.stringify(rs))
-
-function zeroFill( number, width )
-{
-  width -= number.toString().length;
-  if ( width > 0 )
-  {
-    return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
-  }
-  return number + ""; // always return a string
-}
-
 
 data.map(d => {
   if(d.boardgame_id !== 23953) {
     if(d.boardgame_id !== null) {
       const date = new Date(d.cr_date)
 
-      console.log(util.format('%d,%s,%f', d.boardgame_id, date.toISOString().split("T")[0], d.price))
+      console.log(util.format('%d,%s,%f,%d,%s', d.boardgame_id, date.toISOString().split("T")[0], d.price, d.stock, d.store_name))
     }
   }
 })
+
+//for i in {1..22}; do git reset master~1 --hard; [[ -f prices.json.gz ]] && gunzip prices.json.gz; node /tmp/pricesToCSV.js > /tmp/asdf$i.csv; rm prices.json; done
